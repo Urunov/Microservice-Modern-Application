aa550813c4a47084bc87fe6ef43f4547433780fa
Hamdamboy <hamdamboy.urunov@gmail.com>
2020-12-09 18:03:12 +0900

    Job Scheduler FullStack Spring Boot Angular JS

---
+++ Project-0.JobScheduling/src/main/java/com/urunov/model/Jobs.java
@@ -0,0 +1,19 @@
+package com.urunov.model;
+
+import java.util.Date;
+
+/**
+ * User: hamdamboy
+ * Project: JobSchdule
+ * Github: @urunov
+ */
+public class Jobs {
+
+    private Long id;
+
+    private String TaskName;
+
+    private Date period;
+
+
+}
---
+++ Project-0.JobScheduling/src/main/java/com/urunov/service/JobService.java
@@ -0,0 +1,52 @@
+package com.urunov.service;
+
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * User: hamdamboy
+ * Project: JobSchdule
+ * Github: @urunov
+ */
+public interface JobService {
+
+    // Schedule one time job.
+    boolean scheduleOneTimeJob(String jobName, Class<? extends QuartzJobBean> jobClass, Date date);
+
+    // Schedule cron recurring job.
+    boolean scheduleCronJob(String jobName, Class<? extends QuartzJobBean> jobClass, Date date, String cronExpression);
+
+    // Update one time recurring job.
+    boolean updateCronJob (String jobName, Date date, String cronExpression);
+
+    //Unschedule scheduled job.
+    boolean unScheduledJob (String jobName);
+
+    // Delete a job
+    boolean deleteJob(String jobName);
+
+    // Pause job
+    boolean pauseJob(String jobName);
+
+    // resume a job
+    boolean resumeJob(String jobName);
+
+    // Starting a job
+    boolean startJobNow(String jobName);
+
+    List<Map<String, Object>> getAllJobs();
+
+    // Get if job is already Running.
+    boolean isJobWithNamePresent(String jobName);
+
+    // Get the current state of job.
+    String getJobState(String jobName);
+
+    // Step a job
+    boolean stopJob(String jobName);
+
+}
